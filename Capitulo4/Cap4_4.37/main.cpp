9aaddbbc7f50fc191b124ec9c184a3d0e1c24d87
Ruddyard_Castro <rcastroc2@miumg.edu.gt>
2024-02-18 13:30:38 -0600

    Cap 4

---
+++ Capitulo4/Cap4_4.37/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+// Uso year porqu años no se puede
+// Uso size por que la poblacion no se duplico
+using namespace std;
+
+int main()
+{
+ long long int poblacionMundial =8100000000;
+  const float tasaDeCrecimiento =0.0091;
+  long long int poblacionactual=poblacionMundial;
+
+cout<<"Year"<<"\t"<<"Size de pobalcion"<<"\t"<<"Aumento de poblacion"<<endl;
+  for (int i=1;i<75;i++){
+
+
+        poblacionMundial=poblacionMundial+(poblacionMundial*tasaDeCrecimiento);
+
+        poblacionactual=poblacionMundial-poblacionactual;
+
+
+
+        cout<<i<<"\t"<<poblacionMundial<<"\t""\t"<<poblacionactual<<"\n"<<endl;
+
+  }
+if  (poblacionMundial==(2*poblacionMundial)){
+
+    cout<<"La poblacion se duplico"<<endl;
+}else {
+ cout<<"La poblacion no  se duplico"<<endl;
+}
+}
